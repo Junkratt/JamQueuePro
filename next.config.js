ae41869f87584bf3cb1cec0ac57e200196e7f815
Jarratt VanDyke <junkratt@gmail.com>
2025-08-26 07:28:26 -0700

    Initialize Tailwind CSS properly

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   experimental: {
     esmExternals: false,
-  },
-  // Disable static optimization completely
-  trailingSlash: false,
-  generateEtags: false,
-  poweredByHeader: false,
+  }
 }
 
 module.exports = nextConfig
